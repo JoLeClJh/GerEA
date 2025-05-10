8f1320f43bc59b7e6d84576eaf16df1a2f768899
thetechuld <jonas.winter2010aw@gmail.com>
2025-05-10 12:03:26 +0200

    Authservice

--- android/app/src/main/kotlin/com/example/gerea/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/gerea/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gerea
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
